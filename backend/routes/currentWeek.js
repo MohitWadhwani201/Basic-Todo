66bf4c6e7d3b8835a3f4b7358bfa0c935b1a539b
MohitWadhwani201 <mohit.wadhwani2.01@gmail.com>
2025-12-15 02:59:53 +0530

    updated reset route

--- backend/routes/currentWeek.js
+++ backend/routes/currentWeek.js
@@ -2,26 +2,24 @@ import express from "express";
 import { protect } from "../middleware/authMiddleware.js";
 import User from "../models/User.js";
 const router = express.Router();
+export const getCurrentWeek = async (req, res) => {
+	const user = await User.findById(req.user.id);
+
+	if (!user.cycleStartDate) {
+		user.cycleStartDate = new Date();
+		await user.save();
+	}
+
+	const start = new Date(user.cycleStartDate);
+	const now = new Date();
+
+	const diffDays = Math.floor((now - start) / (1000 * 60 * 60 * 24));
+
+	const weekIndex = Math.floor(diffDays / 7) % 4; // 0–3
+	const dayIndex = diffDays % 7; // 0–6
+
+	res.json({ weekIndex, dayIndex });
+};
 
 router.get("/", protect, getCurrentWeek);
-export const getCurrentWeek = async (req, res) => {
-  const user = await User.findById(req.user.id);
-
-  if (!user.cycleStartDate) {
-    user.cycleStartDate = new Date();
-    await user.save();
-  }
-
-  const start = new Date(user.cycleStartDate);
-  const now = new Date();
-
-  const diffDays = Math.floor(
-    (now - start) / (1000 * 60 * 60 * 24)
-  );
-
-  const weekIndex = Math.floor(diffDays / 7) % 4; // 0–3
-  const dayIndex = diffDays % 7; // 0–6
-
-  res.json({ weekIndex, dayIndex });
-};
 export default router;
