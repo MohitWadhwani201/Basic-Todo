7e673fe15cf0efc917ade09b5e3c79974fa76e82
MohitWadhwani201 <mohit.wadhwani2.01@gmail.com>
2025-12-14 22:21:52 +0530

    just added base api

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,9 @@ connectDB();
 const app = express();
 app.use(cors());
 app.use(express.json());
-
+app.use("/", (req, res) => {
+	res.send("API is running...");
+});
 app.use("/api/auth", authRoutes);
 app.use("/api/tasks", taskRoutes);
 app.use("/api/habits", habitRoutes);
